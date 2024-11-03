654fec3334e29ce86c9fb7e7be5a704b8cf81424
Andressa Colin <104472710+AndressaColin@users.noreply.github.com>
2024-11-03 18:21:37 -0400

    Update tipoPrimitivos.java

--- tipoPrimitivos.java
+++ tipoPrimitivos.java
@@ -1,10 +1,11 @@
 public class Main
 {
-	public static void main(String[] args) {
-    	Scanner leitura = new Scanner(System.in);
+	public static void main(String[] args) 
+	{
+    		Scanner leitura = new Scanner(System.in);
 
-    	System.out.println("Insira um tipo primitivo: ");
-    	String tipo = leitura.nextLine();
-    	System.out.println(tipo);
+    		System.out.println("Insira um tipo primitivo: ");
+    		String tipo = leitura.nextLine();
+    		System.out.println(tipo);
 	}
 }
